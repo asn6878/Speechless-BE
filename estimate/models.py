fdb787beeac5d1e9bcbeb552c25f9726e467d760
Syoon <asn6878@gmail.com>
2023-08-01 21:45:40 +0900

    feat : user model 설계

--- estimate/models.py
+++ estimate/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from user.models import CustomUser as User
 
 # Create your models here.
+
+class Estimate(models.Model):
+    estimate_id = models.BigAutoField(primary_key=True, unique=True, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    video = models.FileField(upload_to='estimate/%Y/%m/%d', blank=True)
+    content = models.TextField(blank=True)
+    dead_line = models.DateTimeField(blank=True)
+    status = models.IntegerField(default=0)
+
+class Offer(models.Model):
+    offer_id = models.BigAutoField(primary_key=True, unique=True, editable=False)
+    estimate_id = models.ForeignKey(Estimate, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    content = models.TextField(blank=True)
+
+class Bookmark(models.Model):
+    bookmark_id = models.BigAutoField(primary_key=True, unique=True, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    estimate_id = models.ForeignKey(Estimate, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    tag_id = models.BigAutoField(primary_key=True, unique=True, editable=False)
+    estimate_id = models.ForeignKey(Estimate, on_delete=models.CASCADE)
+    tag = models.CharField(max_length=20)
