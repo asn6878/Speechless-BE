8423bd10710fb250d7c2b229b4236629225b54f6
asn6878 <asn6878@gmail.com>
2023-08-05 04:11:17 +0900

    feat : 메소드 별로 다른 권한 부여 기능 구현

--- user/views.py
+++ user/views.py
@@ -1,20 +1,26 @@
 # drf 관련 라이브러리
 from rest_framework import viewsets
 from django.contrib.auth.hashers import make_password
+from rest_framework import exceptions
 
 # drf 권한 관련 라이브러리
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from .authentication import JWTAuthentication
 
 # serializers.py
 from .serializers import UserSerializer, UserCreateSerializer, UserInfoSerializer
 
-# models.py
-from .models import CustomUser
+
+# User 모델
+from django.contrib.auth import get_user_model, authenticate
+
+User = get_user_model()
+
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = CustomUser.objects.all()
+    queryset = User.objects.all()
 
-    # 기능별로 다른 시리얼라이저를 사용!
+    # 기능별로 다른 시리얼라이저를 사용
     def get_serializer_class(self):
         if self.action == 'create':
             return UserCreateSerializer
@@ -27,21 +33,30 @@ class UserViewSet(viewsets.ModelViewSet):
         elif self.action == 'destroy' :
             return UserSerializer
         else:
-            print("5개의 기능중 그 무엇도 아닌 이상한 요청이 들어옴.")
-            print(self.action)
-            return UserSerializer
+            # 잘못된 요청임을 알리는 예외 발생
+            raise exceptions.MethodNotAllowed(self.request.method)
+            
+    # 기능별로 다른 권한을 사용 
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        elif self.action == 'retrieve':
+            return [AllowAny()]
+        elif self.action == 'update' :
+            return [IsAuthenticated()]
+        elif self.action == 'list' :
+            return [IsAdminUser()]
+        elif self.action == 'destroy' :
+            return [IsAuthenticated()]
 
-    # 각 기능을 커스터마이징 하기위해 오버라이딩 해줬다.
+    # 각 기능을 커스터마이징(여기서는 비밀번호 암호화) 하기위해 오버라이딩 해줬다.
     def create(self, request, *args, **kwargs):
-        permission_classes = [AllowAny]
         instance = request.data.get('password')
         request.data['password'] = make_password(instance)
 
         return super().create(request, *args, **kwargs)
     
     def update(self, request, *args, **kwargs):
-        permission_classes = [IsAuthenticated]
-
         instance = request.data.get('password')
         request.data['password'] = make_password(instance)
 
