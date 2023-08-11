e06205132cdebe5b1dda94c9e3a2f60dafac1a40
DinoDeveloper <asn6878@gmail.com>
2023-08-12 03:38:48 +0900

    feat : offer 부분 설계 (환경 이동)

---
+++ offer/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+# 모델 관련 라이브러리
+from estimate.models import Estimate
+from django.contrib.auth import get_user_model
+from .models import Offer
+
+User = get_user_model()
+
+class OfferDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Offer
+        fields = [
+            'user_id',
+            'title',
+            'video',
+            'content',
+            'price',
+            'created_at',
+            'status',
+        ]
--- offer/views.py
+++ offer/views.py
@@ -1,3 +1,27 @@
+# drf 관련 라이브러리
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import viewsets, exceptions, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+# drf 권한 관련 라이브러리
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+# models.py
+from estimate.models import Estimate
+from .models import Offer
+
+# serializers.py
+
+
+class OfferView(APIView):
+    # pk값의 견적서 안에있는 입찰제안서 목록 조회
+    def get(self, request, pk):
+        permission_classes = [AllowAny]
+        offers = Offer.objects.all()
+        serializer = OfferListSerializer(offers, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, pk):
+        permission_classes = [IsAuthenticated]
